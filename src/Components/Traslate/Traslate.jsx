0058aae9ed546391c3d47643bddb1fb062ef5c43
Mateo Carmelo <analistaprogramador.b@corpacam.com.gt>
2023-10-12 23:52:02 -0600

    UPdated changes

--- src/Components/Traslate/Traslate.jsx
+++ src/Components/Traslate/Traslate.jsx
@@ -8,23 +8,27 @@ import {
   SafeAreaView,
 } from "react-native";
 import numerosChuj from "../Service/DB.json";
+import { API_Services } from "../../Config/env";
 
 function Traslate() {
   const [inputText, setInputText] = useState("");
   const [submitButton, setSubmitButton] = useState("");
   const [numeros, setNumeros] = useState([]);
-  //   console.log(numerosChuj["B'islab'_Chuj");
+
+  const [inputText2, setInputText2] = useState("");
+  const [mostrarPalabras, setMostrarPalabras] = useState("");
+  const [palabras, setPalabras] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // const response = await fetch("http://localhost:3434/api/numerosChuj");
-        const response = await fetch(
-          "https://api-clchuj.onrender.com/api/numerosChuj"
-        );
-        const data = await response.json();
-        console.table(data);
-        setNumeros(data);
+        const response1 = await fetch(`${API_Services}/numerosChuj`);
+        const data1 = await response1.json();
+        setNumeros(data1);
+
+        const response2 = await fetch(`${API_Services}/palabrasChuj`);
+        const data2 = await response2.json();
+        setPalabras(data2);
       } catch (error) {
         console.error(error.message);
       }
@@ -46,6 +50,19 @@ function Traslate() {
       // const tralado = numerosChuj["B'islab'_Chuj"].find((item) => item.id === id);
       // console.log(tralado.name);
       return tralado ? tralado.name : "Número no encontrado";
+    }
+  };
+
+  const handleClickPalabras = () => {
+    setMostrarPalabras(inputText2);
+  };
+ 
+  const mostrarPalabra = () => {
+    if (palabras && palabras.datos) {
+      const tralado = palabras.datos.find(
+        (item) => item.nombrePalabra === mostrarPalabras
+      );
+      return tralado ? tralado.traduccionPalabra : "Palabra no encontrado";
     }
   };
 
@@ -72,7 +89,7 @@ function Traslate() {
           value={inputText}
           onChangeText={(value) => setInputText(value)}
           placeholder="Ingrese un numero para traducir"
-          keyboardType="numeric"
+          keyboardType="number-pad"
         />
         <TouchableOpacity style={styles.button} onPress={handleButtonClick}>
           <Text style={styles.buttonText}>Traducir números</Text>
@@ -83,20 +100,20 @@ function Traslate() {
         ) : null}
       </View>
 
-      <View>
+      <View style={{ marginTop: 100 }}>
         <TextInput
           style={[styles.input, { marginBottom: 20 }]}
-          value={inputText}
-          onChangeText={(value) => setInputText(value)}
-          placeholder="Ingrese un numero para traducir"
+          value={inputText2}
+          onChangeText={(value) => setInputText2(value)}
+          placeholder="Ingresa alguna palabra"
           keyboardType="numeric"
         />
-        <TouchableOpacity style={styles.button} onPress={handleButtonClick}>
+        <TouchableOpacity style={styles.button} onPress={handleClickPalabras}>
           <Text style={styles.buttonText}>Traducir palabras</Text>
         </TouchableOpacity>
-        {/* <Text style={styles.textoAbajo}>{submitButton}</Text> */}
-        {submitButton ? (
-          <Text style={styles.textoAbajo}>📝📖👉: {mostrarNumeros()}</Text>
+
+        {mostrarPalabras ? (
+          <Text style={styles.textoAbajo}>📝📖👉: {mostrarPalabra()}</Text>
         ) : null}
       </View>
     </SafeAreaView>
@@ -105,16 +122,16 @@ function Traslate() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     // marginTop: 30,
     // justifyContent: "center",
     alignItems: "center",
-    padding: 20,
+    paddingTop: 80,
     backgroundColor: "#f9f9f9",
   },
   input: {
-    width: "80%",
-    height: 40,
+    width: 400,
+    height: 60,
     borderWidth: 1,
     borderColor: "#ccc",
     paddingHorizontal: 10,
@@ -126,9 +143,9 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: "#4a90e2",
-    padding: 10,
+    padding: 15,
     borderRadius: 8,
-    width: "80%",
+    // width: "80%",
   },
   buttonText: {
     color: "white",
